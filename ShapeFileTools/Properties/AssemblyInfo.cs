d1b9d002d0df03184c49a81f64727b07466af218
wfletcher <wfletcher_au@hotmail.com>
2020-11-27 13:22:57 +1100

    4.6.5 release
    updating assemblyInfo

--- ShapeFileTools/Properties/AssemblyInfo.cs
+++ ShapeFileTools/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("4.6.4.1")]
-[assembly: AssemblyFileVersion("4.6.4.1")]
+[assembly: AssemblyVersion("4.6.5.0")]
+[assembly: AssemblyFileVersion("4.6.5.0")]
