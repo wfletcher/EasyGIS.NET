601464a8816136229f67cffb02d750be5e2a4be7
wfletcher <wfletcher_au@hotmail.com>
2022-01-11 00:19:49 +1100

    update copyright in settings

--- ShapeFileTools/Properties/Settings.Designer.cs
+++ ShapeFileTools/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace egis.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.9.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.10.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -26,7 +26,7 @@ namespace egis.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute(@"Easy GIS .NET Desktop Edition
-Copyright (C) 2008 - 2021 Winston Fletcher.
+Copyright (C) 2008 - 2022 Winston Fletcher.
 All rights reserved.
 
 Easy GIS .NET is free software: you can redistribute it and/or modify it under the terms of thes GNU Lesser General Public License version 3 as published by the Free Software Foundation.
