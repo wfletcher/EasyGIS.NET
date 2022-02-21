16b141ec7a59d24e58632c2214a155503370be0b
wfletcher <wfletcher_au@hotmail.com>
2022-02-21 22:52:20 +1100

    updating AssemblyInfo for release

--- ShapeFileTools/Properties/AssemblyInfo.cs
+++ ShapeFileTools/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("4.7.2.0")]
-[assembly: AssemblyFileVersion("4.7.2.0")]
+[assembly: AssemblyVersion("4.7.3.0")]
+[assembly: AssemblyFileVersion("4.7.3.0")]
